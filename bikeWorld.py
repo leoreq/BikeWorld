06cba173aae7c1a108dc41e8788cf86a782d5446
Leonardo Requena <leoreq@gmail.com>
2015-08-26 22:55:58 +0000

    this is the first commit

---
+++ bikeWorld.py
@@ -0,0 +1,14 @@
+class bike(object):
+    """"This class will model bike pricing behaviour"""
+    def ___init___(self,name,weight):
+        self.name=name
+        self.weight=self.weight
+     
+    #def checkWeight(self,insertWeight):
+     #   while insertWeight<21.6:
+      #      reinsertedWeight=raw_input(" weight has to be in pounds and above 21.6 , please insert again")
+       #     insertWeight=reinsertedWeight
+        #return insertWeight    
+SD=bike("SD",24)
+
+print(SD.weight)
