85c58bf7a0bfec7d8190aa743840aeac45713684
Leonardo Requena <leoreq@gmail.com>
2015-08-27 04:07:11 +0000

    added cash invested in store, and allow to buy only if money allows

--- bikeWorld.py
+++ bikeWorld.py
@@ -21,26 +21,31 @@ class Bike(object):
         return price
 
 class BikeStore(object):
-    def __init__(self,name):
+    def __init__(self,name,cash):
         self.name=name
+        self.cash=cash
         inventory=dict()
         self.inventory=inventory
         print""
-        print ("{} has been opened for the bike business").format(self.name)
+        print ("{} has been invested in the bike industry , and finally now {} is open for business").format(self.cash,self.name)
         
     def buy(self,bike):
         """this will update the inventory dictionary with the model,n price. We can then worry about adding same types"""
         ##print("DEBUGGING: check if the bike object info is accesible. The bike name is {}").format(bike.model)
-        modelCount=self.inventory.get(bike.model,[0,0])[1]
-        ##print("DEBUGGING the list within the dict:Value is {}").format(modelCount)
-        self.inventory[bike.model]=[bike.price,modelCount+1]
-        #Assumtion: We will be using "market Price", last price will be set for the whole stock as current price"
-        print""
-        print("Nice Purchase from {} store. \nIt now has {} bikes of - {} model - ,priced at {} each").format(self.name,self.inventory[bike.model][1],bike.model,self.inventory[bike.model][0])
+        if (bike.price>self.cash):
+            print(" Not enough CASH for the operation. You have {} and need {} ").format(self.cash,bike.price)
+        else:
+            self.cash-=bike.price
+            modelCount=self.inventory.get(bike.model,[0,0])[1]
+            ##print("DEBUGGING the list within the dict:Value is {}").format(modelCount)
+            self.inventory[bike.model]=[bike.price,modelCount+1]
+            #Assumtion: We will be using "market Price", last price will be set for the whole stock as current price"
+            print""
+            print("Nice Purchase from {} store. \nIt now has {} bikes of - {} model - ,priced at {} each").format(self.name,self.inventory[bike.model][1],bike.model,self.inventory[bike.model][0])
 
 SD=Bike("SD",22)
 
-BikeLandia=BikeStore("Bikelandia")
+BikeLandia=BikeStore("Bikelandia",5000)
 BikeLandia.buy(SD)
 BikeLandia.buy(SD)
 SD=Bike("Plaza",23)
