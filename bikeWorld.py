c2a7e11a4d00da6e35f22e9876c13e33286051e2
Leonardo Requena <leoreq@gmail.com>
2015-08-28 04:45:25 +0000

    added the Client profile and completed remainings of basic sollution

--- bikeWorld.py
+++ bikeWorld.py
@@ -44,10 +44,12 @@ class BikeStore(object):
             #Assumtion: We will be using "market Price", last price will be set for the whole stock as current price"
             print""
             print("Nice Purchase from {} store. \nIt now has {} bikes of - {} model - ,priced at {} each").format(self.name,self.inventory[bike.model][1],bike.model,self.inventory[bike.model][0])
-    
+            self.printInventory()
+            
     def sell(self,model):
         modelPrice=self.inventory.get(model,[0,0])[0]
         modelCount=self.inventory.get(model,[0,0])[1]
+        print""
         if (modelCount==0):
             print("{} model is not in stock. Please check our list of available items.").format(model)
         else:
@@ -56,18 +58,62 @@ class BikeStore(object):
             self.profit+=(markupPrice-modelPrice)
             self.cash+=markupPrice
             print("Model {} - has been sold for {}, leaving only {} in stock. The profit of the operation is {} and the company now has {} of cash.").format(model,markupPrice,self.inventory[model][1],(markupPrice-modelPrice),self.cash)
-            
-SD=Bike("SD",22)
+            self.printInventory()
+    def printInventory(self):
+        print""
+        print ("{} inventory is now:").format(self.name)
+        for model,priceList in self.inventory.iteritems():
+            print("Model: {} || Price: {} || Quantity: {} ").format(model,priceList[0],priceList[1])
 
-BikeLandia=BikeStore("Bikelandia",5000)
+class Client(object):
+    """This class will represent the behaviour of a bike lover. Will buy the best bike he can afford."""
+    def __init__(self,name,cash):
+        self.name=name
+        self.cash=cash
+        self.assets=dict()
+        print ""
+        print ("We welcome {} to this bike lovin block. You have {} to spend as you wish.").format(self.name,self.cash)
+    
+    def buy(self,store):
+        """Buying function will check all items, and choose the most expensive it can afford."""
+        modelChosen=""
+        priceChosen=0
+        for model,priceList in store.inventory.iteritems():
+            if (priceList[1]<=0) or (priceList[0]>self.cash) or (priceChosen>=priceList[0]):#this condition looks to check the price only if there is ammount of stock to sell and price can be aforded.
+                continue
+            else:
+               priceChosen=priceList[0]
+               modelChosen=model
+        if (modelChosen!=""):
+            self.cash-=priceChosen
+            self.assets[modelChosen]=[priceChosen]
+            print ""
+            print("{} has just added a {} to his ownership. He spent {} and now has {} left.").format(self.name,modelChosen,priceChosen,self.cash)
+            store.sell(modelChosen)
+        else: print("Sorry {}. {} has no bike that fits you, please come back again !!").format(self.name,store.name)
+        
+#Bike Stocking
+SD=Bike("SD",23.5)
+BikeLandia=BikeStore("Bikelandia",10000)
 BikeLandia.buy(SD)
 BikeLandia.buy(SD)
-SD=Bike("Plaza",23)
-BikeLandia.buy(SD)
+Plaza=Bike("Plaza",24)
+BikeLandia.buy(Plaza)
+Downtown=Bike("Downtowm",25.5)
+BikeLandia.buy(Downtown)
 
-print(BikeLandia.inventory)
+#Open for business
+Dan=Client("Dan",200)
+Dan.buy(BikeLandia)
 
-BikeLandia.sell("SD")
-print(BikeLandia.inventory)
-BikeLandia.sell("SD")
+ultraDowntown=Bike("ultraDowntown",29)
+BikeLandia.buy(Downtown)
+BikeLandia.buy(ultraDowntown)
 
+Dan.buy(BikeLandia)
+
+Leo=Client("Leonidas",1000)
+Leo.buy(BikeLandia)
+
+Locaso=Client("Locaso",500)
+Locaso.buy(BikeLandia)
